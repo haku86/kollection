a30ddfacadf03c4bf6ed4edba8601b9e7be860e8
Joel <lomyril86@gmail.com>
2012-12-24 16:08:28 +0800

    added boostrap.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Kollection::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery.barousel.js )
+  config.assets.precompile += ['jquery.barousel.js', 'bootstrap.js', 'custom.css.scss']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
